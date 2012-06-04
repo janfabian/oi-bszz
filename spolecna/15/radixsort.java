4409f25a353f87b2a47a60ab7b7c119a9a90c42a
janfabian <jan.fabian@jetminds.com>
2012-06-04 17:03:09 +0200

    15 A4B36ALG

--- spolecna/15/radixsort.java
+++ spolecna/15/radixsort.java
@@ -1,30 +1,44 @@
 void radix_sort (String [] a) {
   int len   = ...; // number of chars used (2^16?)
-  int [] z  = new int [len]; int [] k  = new int [len]; int [] z1 = new int [len]; int [] k1 = new int [len]; int [] d  = new int [a.length]; int [] d1 = new int [a.length]; int [] aux;
+  int [] z  = new int [len];
+  int [] k  = new int [len];
+  int [] z1 = new int [len];
+  int [] k1 = new int [len];
+  int [] d  = new int [a.length];
+  int [] d1 = new int [a.length];
+  int [] aux;
   initStep(a, z, k, d);       
   for (int p = a[0].length()-2; p >= 0; p-- ) {
     radixStep(a, p, z, k, d, z1, k1, d1); 
     aux = z; z = z1; z1 = aux;      
     aux = k; k = k1; k1 = aux;      
     aux = d; d = d1; d1 = aux;      
-  }            
+  } 
+  output(a, z, k, d);             
 }
 
 void initStep (String[] a, int[] z, int [] k, int[] d){
   int pos = a[0].length()-1;
-  for (int i = 0; i < a.length; i++) {  
-    c = (int) a[i].charAt(pos);         
-    if (z[c] == -1)                        
-      k[c] = z[c] = i;                    
-    else {
+  int c;             
+  for (int i = 0; i < z.length; i++)    
+    z[i] = k[i] = -1; // empty
+    for (int i = 0; i < a.length; i++) {  
+      c = (int) a[i].charAt(pos);         
+      if (z[c] == -1)                        
+      k[c]= z[c] = i;                    
+      else {
       d[k[c]] = i;                 
-      k[c]    = i;
-  } 
+      k[c] = i;
+    } 
   } 
 }
 
 void radixStep(String[]a, int pos, int[] z, int[] k, 
-  int[] d, int[] z1, int[] k1, int[] d1){         
+  int[] d, int[] z1, int[] k1, int[] d1){
+  int j;        
+  int c;        
+  for (int i = 0; i < z.length; i++)  
+  z1[i] = k1[i] = -1;            
   for (int i = 0; i < z.length; i++)  
     if (z[i] != -1) {                 
       j = z[i];                      
